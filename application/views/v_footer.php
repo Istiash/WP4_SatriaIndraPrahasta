d43469cc1ca2cb514b0fb8d9323ccaa3bf015b81
Istiash <sipcuty@gmail.com>
2023-10-16 09:38:42 +0700

    Memasukan file tugas pertemuan 4

---
+++ application/views/v_footer.php
@@ -0,0 +1,7 @@
+<div>
+<footer>
+<a href="<?php echo base_url().'index.php/web' ?>">Rental Buku</a>
+ </footer>
+ </div>
+</body>
+</html>
---
+++ application/views/v_news.php
@@ -0,0 +1,24 @@
+<body>
+<section>
+    <h1><?php echo $judul ?></h1>
+    <div class="img">
+        <img src="https://th.bing.com/th?id=OIP.408K2peqE2He82hYSy9l2gHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2q=90" alt="">
+    </div>
+    <h2>Kampung durian Runtuh</h2>
+    <p>Dr durian, 12 Maret 969 - Musim panen durian telah tiba di Kampung Durian Runtuh, dan warga desa ini tengah merayakan keberuntungan mereka dengan hasil panen yang melimpah. Durian, buah raja yang lezat, merupakan sumber penghasilan utama bagi penduduk kampung ini, dan tahun ini tampaknya menjadi tahun yang sangat baik.</p>
+
+<p>Kampung Durian Runtuh, terletak di lereng bukit yang hijau subur, dikenal sebagai "Kampung Durian" karena buah durian yang tumbuh di sini dikenal memiliki rasa yang istimewa dan kualitas yang sangat baik. Masyarakat kampung ini menggantungkan hidup mereka pada pertanian durian, dan musim panen adalah saat-saat yang paling dinanti-nantikan dalam setahun.</p>
+
+<p>Penduduk kampung mengatakan bahwa cuaca yang mendukung sepanjang tahun ini telah menciptakan kondisi ideal bagi pohon durian untuk berbuah dengan baik. Banyak petani telah melaporkan hasil panen yang melimpah, dan buah durian yang besar dan beraroma telah ditemukan di hampir setiap kebun durian.</p>
+
+<p>"Kami sangat bersyukur tahun ini. Durian-durian ini tampak lebih sempurna daripada sebelumnya. Cuaca mendukung dan hujan turun cukup banyak, memberikan nutrisi yang baik bagi pohon durian," kata Pak Surya, seorang petani durian setempat.</p>
+
+<p>Selain bermanfaat bagi petani, musim panen durian juga telah meningkatkan sektor pariwisata di kampung ini. Banyak wisatawan lokal maupun mancanegara datang ke Kampung Durian Runtuh untuk menikmati buah durian segar dan berpartisipasi dalam berbagai acara terkait durian.</p>
+
+<p>Kepala Desa, Ibu Susanti, mengatakan, "Musim panen durian adalah saat yang menggembirakan bagi seluruh kampung. Selain memberikan kesempatan ekonomi yang baik bagi warga, kami juga membuka pintu kami untuk wisatawan yang ingin merasakan keindahan alam dan kulinernya."</p>
+
+<p>Acara-acara seperti festival durian, lomba makan durian, dan kunjungan ke kebun durian telah menjadi daya tarik utama bagi wisatawan yang datang ke Kampung Durian Runtuh. Selain itu, beberapa pedagang lokal juga membuka warung durian sementara untuk memanjakan pengunjung dengan berbagai hidangan durian yang lezat.</p>
+
+<p>Meskipun tahun ini tampaknya menjadi tahun yang sangat baik bagi Kampung Durian Runtuh, warga setempat dan petani berharap bahwa hasil panen yang baik ini akan berlanjut di masa depan. Dengan kerja keras dan perawatan yang baik terhadap pohon durian, mereka berharap agar keberuntungan ini dapat berlanjut dalam beberapa tahun ke depan.</p>
+</section>
+</body>
